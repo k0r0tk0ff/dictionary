72193ab7a7c63a7c8d7c4ee3fca808e309d50988
k0r0tk0ff <korotkov34reg@gmail.com>
2018-01-07 21:01:33 +0300

    add properties and log. Do not work log! (2)

--- web/src/main/java/servlets/MainServlet.java
+++ web/src/main/java/servlets/MainServlet.java
@@ -3,6 +3,7 @@ package servlets;
 import businesslogic.YandexApiExe;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
@@ -12,7 +13,31 @@ import javax.servlet.http.HttpServletResponse;
 
 public class MainServlet extends HttpServlet {
 
-    private static final Logger log = Logger.getLogger(MainServlet.class.getName());
+    //private static final Logger log = Logger.getLogger(MainServlet.class.getName());
+    private Logger log;
+
+    public void init() {
+        this.log = Logger.getLogger(MainServlet.class);
+
+/*        String realPath = getServletContext().getRealPath("/");
+        String fileSep = System.getProperty("file.separator");
+
+        if (realPath != null && (!realPath.endsWith(fileSep)))
+            realPath = realPath + fileSep;
+
+        System.out.println(realPath);
+
+        //load the configuration for this application's loggers using the
+        // servletLog.properties file
+        PropertyConfigurator.configure(realPath
+                + "WEB-INF/classes/servletLog.properties");
+        //create the logger for this servlet class
+        //it will use the configuration for the logger com.java2s.LoggerServlet
+        //or inherit from the logger com.java2s if one exists, and so on
+        log = Logger.getLogger(MainServlet.class);
+        log.info("LoggerServlet started.");*/
+
+    }
 
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -24,12 +49,16 @@ public class MainServlet extends HttpServlet {
         String result = null;
         request.setCharacterEncoding("UTF-8");
 
-            try {
+        log.debug(" DEBUG Test MainServlet debug before error");
+        log.info("Sending an INFO message");
+        log.fatal("Sending an fatal message");
+
+
+        try {
                 //yaApi = YandexApiExe.getInstance();
                 //result = yaApi.doGetTranslatedWord(request.getParameter("wordForTranslate"));
                 throw new Exception("afffff");
             } catch (Exception e) {
-                System.out.println("Intercept!!!");
                 log.error(e.getMessage());
                 log.debug(" Test MainServlet debug");
             }
