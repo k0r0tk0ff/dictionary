4ea77f8e0803dc96b6f74ba720da24518126db77
k0r0tk0ff <korotkov34reg@gmail.com>
2018-02-07 20:25:12 +0300

    work build to single project (use maven assembly plugin)

---
+++ web/src/main/java/servlets/Main.java
@@ -0,0 +1,30 @@
+package servlets;
+
+//import businesslogic.YandexApiExe;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Optional;
+import org.apache.catalina.startup.Tomcat;
+
+public class Main {
+
+    private static final Logger LOG  = LoggerFactory.getLogger(Main.class);
+
+    public static final Optional<String> PORT = Optional.ofNullable(System.getenv("PORT"));
+    public static final Optional<String> HOSTNAME = Optional.ofNullable(System.getenv("HOSTNAME"));
+
+    public static void main(String[] args) throws Exception {
+
+        String contextPath = "/" ;
+        String appBase = ".";
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(Integer.valueOf(PORT.orElse("8080") ));
+        tomcat.setHostname(HOSTNAME.orElse("localhost"));
+        tomcat.getHost().setAppBase(appBase);
+        tomcat.addWebapp(contextPath, appBase);
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+}
--- web/src/main/java/servlets/MainServlet.java
+++ web/src/main/java/servlets/MainServlet.java
@@ -8,9 +8,15 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(
+        name = "MainServlet",
+        urlPatterns = {"/MainServlet"}
+)
 
 public class MainServlet extends HttpServlet {
 
