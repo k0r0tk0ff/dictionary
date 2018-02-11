dabd3dec63731659f9f49e0f3b3c0293ecb1c249
k0r0tk0ff <korotkov34reg@gmail.com>
2018-02-11 17:57:12 +0300

    add db tests

--- db/src/main/java/DbConnector.java
+++ db/src/main/java/DbConnector.java
@@ -11,6 +11,7 @@ public class DbConnector {
     private static Properties properties;
     private InputStream input = null;
 
+    //public void loadDbProperties() {
     private DbConnector() {
         properties = new Properties();
         input = this.getClass().getClassLoader().getResourceAsStream("dbConnect.properties");
@@ -32,17 +33,17 @@ public class DbConnector {
     public Connection connection = null;
 
     public void initializeConnection() throws SQLException {
-/*        connection = DriverManager.getConnection(
-                properties.getProperty("url"),
-                properties.getProperty("user"),
-                properties.getProperty("password")
-                );*/
-
         connection = DriverManager.getConnection(
+                properties.getProperty("jdbc.url"),
+                properties.getProperty("jdbc.user"),
+                properties.getProperty("jdbc.password")
+                );
+
+/*        connection = DriverManager.getConnection(
                 "jdbc:h2:~/test",
                 "sa",
                 ""
-                );
+                );*/
     }
 
     public void closeConnection (Connection connection) throws SQLException {
--- db/src/test/java/DbTest.java
+++ db/src/test/java/DbTest.java
@@ -9,8 +9,6 @@ import static org.junit.Assert.*;
 
 public class DbTest {
 
-   //Logger log  = LoggerFactory.getLogger(DbTest.class);
-
     @Test
     public void loadDriverTestH2() throws Exception {
 
@@ -23,66 +21,41 @@ public class DbTest {
         }
         System.out.println("H2 JDBC Driver Registered.");
     }
-     /*
-    @Test
-    public void loadDriverTest() throws Exception {
-
-        try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            System.out.println("Install PostgreSQL JDBC Driver!");
-            e.printStackTrace();
-            return;
-        }
-        System.out.println("PostgreSQL JDBC Driver Registered.");
-    }*/
 
     @Test
     public void loadPropertiesFromFile() throws Exception {
-
         DbConnector connector = DbConnector.getInstance();
-        assertThat(connector.getPropertyValue("jdbc.username"), is("sa"));
+        assertThat(connector.getPropertyValue("jdbc.user"), is("sa"));
         System.out.println("Read properties from file success.");
     }
 
-    /*
     @Test
-    public void testConnectToDb() throws SQLException {
-
-        SQLException dataAccessError = null;
-        SQLException closeConnectionError = null;
-
-        Connection connection = null;
+    public void showDataFromDb() {
+        String sql = "SELECT * FROM DICTIONARY;";
+        DbConnector connector = DbConnector.getInstance();
 
         try {
-            connection = DriverManager.getConnection(
-                "jdbc:postgresql://127.0.0.1:5432/dictionary",
-                "postgres",
-                "zxcvbnm");
-        } catch (SQLException e1) {
-            dataAccessError = e1;
-        } finally {
-            if (connection != null) {
-                try {
-                    if (connection != null) {
-                       connection.close();
-                    }
-                }
-                catch (SQLException c) {
-                    closeConnectionError = c;
-                }
-            } else {
-                System.out.println(" Connection Failed !");
+            connector.initializeConnection();
+            connector.createDb(connector.getConnection());
+
+            PreparedStatement pr = connector.getConnection().prepareStatement(sql);
+            ResultSet resultSet = pr.executeQuery();
+            while (resultSet.next()) {
+                String eng = resultSet.getString("ENG");
+                String ru = resultSet.getString("RU");
+                System.out.println(String.format("%s | %s", eng, ru));
             }
+
+            System.out.println(resultSet.toString());
+
+            connector.closeConnection(connector.getConnection());
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
 
-        if(dataAccessError != null || closeConnectionError != null) {
-            handleExceptions(dataAccessError, closeConnectionError);
-        } else {
-            System.out.println("DB Connection success.");
-        }
     }
 
+        /*
     @Test
     public void testLogWrite() {
 
@@ -118,32 +91,6 @@ public class DbTest {
         }
         log.debug("done");
     }*/
-
-    @Test
-    public void createDb() {
-        String sql = "SELECT * FROM DICTIONARY;";
-        DbConnector connector = DbConnector.getInstance();
-
-        try {
-            connector.initializeConnection();
-            connector.createDb(connector.getConnection());
-
-            PreparedStatement pr = connector.getConnection().prepareStatement(sql);
-            ResultSet resultSet = pr.executeQuery();
-            while (resultSet.next()) {
-                String eng = resultSet.getString("ENG");
-                String ru = resultSet.getString("RU");
-                System.out.println(String.format("%s | %s", eng, ru));
-            }
-
-            System.out.println(resultSet.toString());
-
-            connector.closeConnection(connector.getConnection());
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-    }
 }
 
 
