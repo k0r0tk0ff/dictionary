a58d6db80cac3fa5cd9bc756c71ceb05e8ba3a52
Alexandr <korotkov34reg@gmail.com>
2018-02-20 16:52:02 +0300

    Update Exe.java

--- exe/src/main/java/exe/Exe.java
+++ exe/src/main/java/exe/Exe.java
@@ -12,28 +12,22 @@ public class Exe {
 
    // private static final Logger LOG  = LoggerFactory.getLogger(Exe.class);
 
+    // private static final Logger LOG  = LoggerFactory.getLogger(Exe.class);
+
     public static final Optional<String> PORT = Optional.ofNullable(System.getenv("PORT"));
     public static final Optional<String> HOSTNAME = Optional.ofNullable(System.getenv("HOSTNAME"));
 
     public static void main(String[] args) throws Exception {
 
-        /*String contextPath = "/" ;
-        String appBase = ".";*/
+        String contextPath = "/" ;
+        String appBase = ".";
         Tomcat tomcat = new Tomcat();
-        /*tomcat.setPort(Integer.valueOf(PORT.orElse("8080") ));
+        tomcat.setPort(Integer.valueOf(PORT.orElse("8080") ));
         tomcat.setHostname(HOSTNAME.orElse("localhost"));
         tomcat.getHost().setAppBase(appBase);
-        tomcat.addWebapp(contextPath, appBase);*/
+        tomcat.addWebapp(contextPath, appBase);
 
-        //------------------------------------------
-
-        Context ctx = tomcat.addContext("/", new File(".").getAbsolutePath());
-
-        Tomcat.addServlet(ctx, "ServletForTranslate", new ServletForTranslate());
-        ctx.addServletMapping("/*", "ServletForTranslate");
-        //ctx.addServletMapping("/ServletForTranslate", "ServletForTranslate");
         tomcat.start();
-        //------------------------------------------
 
         tomcat.getServer().await();
     }
